67b59aae04ad108c5a5bc31f0c90b6191ed1c620
vaddurivineel <89639959+vaddurivineel@users.noreply.github.com>
2022-11-28 16:21:35 +0530

    Update tomcat-installation.sh

--- tomcat-installation.sh
+++ tomcat-installation.sh
@@ -12,23 +12,23 @@ sudo apt-get update -y 1>/dev/null
 sudo apt-get install -y default-jre 1>/dev/null
 echo "            -> Done"
 
-# Downloading Apache Tomcat 9.0.68 version to OPT folder
-echo "**********Downloading Apache Tomcat 9.0.68 version"
+# Downloading Apache Tomcat 9.0.69 version to OPT folder
+echo "**********Downloading Apache Tomcat 9.0.69 version"
 cd /opt
 sudo systemctl stop tomcat 2>/dev/null
 sudo rm -rf apache* tomcat*
 sudo mkdir -p /opt/tomcat
-sudo wget -q https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.68/bin/apache-tomcat-9.0.68.tar.gz
-sudo tar xf apache-tomcat-9.0.68.tar.gz -C /opt/tomcat 1>/dev/null
-sudo rm -rf apache-tomcat-9.0.68.tar.gz
+sudo wget -q https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.69/bin/apache-tomcat-9.0.69.tar.gz
+sudo tar xf apache-tomcat-9.0.69.tar.gz -C /opt/tomcat 1>/dev/null
+sudo rm -rf apache-tomcat-9.0.69.tar.gz
 echo "            -> Done"
 
 # Configuring Tomcat server for manager, host-manager and tomcat users
 echo "**********Configuring Tomcat server for Manager, Host-manager and Credentials"
 cd - 1>/dev/null
-sudo cp context.xml /opt/tomcat/apache-tomcat-9.0.68/webapps/manager/META-INF/context.xml
-sudo cp context.xml /opt/tomcat/apache-tomcat-9.0.68/webapps/host-manager/META-INF/context.xml
-sudo cp tomcat-users.xml /opt/tomcat/apache-tomcat-9.0.68/conf/tomcat-users.xml
+sudo cp context.xml /opt/tomcat/apache-tomcat-9.0.69/webapps/manager/META-INF/context.xml
+sudo cp context.xml /opt/tomcat/apache-tomcat-9.0.69/webapps/host-manager/META-INF/context.xml
+sudo cp tomcat-users.xml /opt/tomcat/apache-tomcat-9.0.69/conf/tomcat-users.xml
 echo "            -> Done"
 
 # Configuring Tomcat as a Service
